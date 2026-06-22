0c42414808bfa92c4b5ed418b50615a536b8c4ac
unknown <harishma1728@gmail.com>
2026-06-22 22:43:50 +0530

    Fix recipe image

--- src/main.ts
+++ src/main.ts
@@ -97,7 +97,7 @@ const response = await groq.chat.completions.create({
 });
 const recipe = response.choices[0].message.content || "";
 const foodImage =
-"https://images.unsplash.com/photo-1504674900247-0877df9cc836?w=800";
+"https://picsum.photos/800/500";
 result!.innerHTML = `
 <div
 style="

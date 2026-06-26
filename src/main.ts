3f96801fe9db07cfce442fbff724d73f420737f5
unknown <harishma1728@gmail.com>
2026-06-26 14:34:57 +0530

    Update recipe generator

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import Groq from "groq-sdk";
+console.log(import.meta.env.VITE_GROQ_API_KEY);
 const groq = new Groq({
   apiKey: import.meta.env.VITE_GROQ_API_KEY,
   dangerouslyAllowBrowser: true,
